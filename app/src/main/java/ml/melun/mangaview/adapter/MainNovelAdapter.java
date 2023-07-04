03b31b7a47bb56810f414668410a6cef3113032b
노니 <ojc123004@gmail.com>
2023-07-05 03:48:08 +0900

    first commit

---
+++ app/src/main/java/ml/melun/mangaview/adapter/MainNovelAdapter.java
@@ -0,0 +1,29 @@
+package ml.melun.mangaview.adapter;
+
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import ml.melun.mangaview.R;
+import org.jetbrains.annotations.NotNull;
+
+public class MainNovelAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+    @NonNull
+    @NotNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+}
